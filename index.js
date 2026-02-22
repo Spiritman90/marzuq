2a61486f469c4372608d33a1c48a8878048460a9
apete <apeter@morphinnovations.com>
2026-02-22 01:04:10 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+// for(x = 0;  x <= 10; x++){
+//     console.log(x)
+// }
+
+
+// function reverseString(reverseMe){
+
+//  let reversed = "";
+//  for(let i = reverseMe.length - 1; i >= 0; i--){
+//    reversed += reverseMe[i]
+//  }
+//  return reversed;
+
+
+// }
+
+
+// console.log(reverseString("Saka"));
+// console.log(reverseString("Martinelli"));
+// console.log(reverseString("Jesus"));
+// console.log(reverseString("Marzuq"));
+
+
+function addTwoNumbers(num1, num2) {
+  let sum = num1 + num2;
+  return sum;
+}
+
+
+console.log(addTwoNumbers(5, 5))
+
+
+
+
+
+
+
+
+
+
+
+
+
